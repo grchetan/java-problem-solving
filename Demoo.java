18d414e73dc71582afeffe8465c67dc3a109e810
grchetan <chetanprajapat340@gmail.com>
2026-02-05 10:21:30 +0530

    new file

---
+++ Demoo.java
@@ -0,0 +1,19 @@
+public class Demoo {
+  public static void main(String[] args) {
+
+
+    int n = 5;
+
+    for(int row = 0; row < n; row++) {
+         for(int col = 0; col < row; col++) {
+
+        System.out.print("* ");
+  }
+  System.out.println();
+  
+    }
+    
+  
+  }
+  
+}
