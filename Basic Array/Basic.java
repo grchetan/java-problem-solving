1da7ef2b9338f811b00beb3d1070ee5ddae1c13d
grchetan <chetanprajapat340@gmail.com>
2026-02-09 09:58:22 +0530

    add files

---
+++ Basic Array/Basic.java
@@ -0,0 +1,25 @@
+
+public class Basic {
+
+  public static void main(String[] args) {
+
+    int[] arr = new int[6];
+    arr[0] = 2;
+    arr[1] = 4;
+    arr[2] = 6;
+    arr[3] = 8;
+    arr[4] = 10;
+     arr[5] = 0;
+
+     for(int i = 0; i < 6; i++){
+      System.out.println(arr[i]);
+     }
+    
+    
+
+
+    
+
+  }
+  
+}
---
+++ Basic Array/Basic2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Basic2 {
+  public static void main(String[] args) {
+
+    int[] even = new int[6];
+    Scanner sc = new Scanner(System.in);
+
+   
+    for(int i = 0; i < 6; i++){
+      System.out.println("Enter a number : " + i);
+      even[i] = sc.nextInt();
+    }
+
+    for(int i = 0; i < 6; i++){
+      
+      System.out.print(even[i]);
+    }
+    
+  }
+  
+}
