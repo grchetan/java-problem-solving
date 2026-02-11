ce4a1c4f638474aaa962dafcba076ca29b4c7a9f
grchetan <chetanprajapat340@gmail.com>
2026-02-11 09:47:06 +0530

    Reverse2 add file

--- Basic Array/Reverse.java
+++ Basic Array/Reverse.java
@@ -1,6 +1,30 @@
 public class Reverse {
+
+  //Optimized Array reverse Array
+
+  public static void Reverse_arr (int[] arr ){ 
+  int i = 0, j = arr.length -1;
+
+  while (i <= j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
+  
+}
+  for(int k = 0; k < arr.length; k++ ){
+    System.out.print(arr[k]);
+  }
+  }
   public static void main(String[] args) {
+
+    int[] arr = {2, 4 , 9, -1 , 0, 8 , 9};
+   
+
+    Reverse_arr(arr);
     
+  
   }
   
 }
---
+++ Basic Array/Reverse2.java
@@ -0,0 +1,8 @@
+public class Reverse2 {
+  public static void main(String[] args) {
+    
+    
+
+  }
+  
+}
