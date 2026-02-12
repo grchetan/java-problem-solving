2a7fa90ba9791758f5e08b09d47ec74839095d94
grchetan <chetanprajapat340@gmail.com>
2026-02-12 12:11:25 +0530

    add file

--- practicess/patterns.java
+++ practicess/patterns.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
 
 
 public class patterns {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a number :");
+    int n = sc.nextInt();
+
+    for(int row = 0 ; row < n; row++){
+      for(int col = 0 ; col < n - row - 1; col++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+
+
+
+    sc.close();
+
+
+  }
+
   
   
 }
