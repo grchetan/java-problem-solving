5eef171b452553f44d14132fdf63358ca939b1bd
grchetan <chetanprajapat340@gmail.com>
2026-02-12 09:46:00 +0530

    add file

--- patterns/patter3.java
+++ patterns/patter3.java
@@ -13,10 +13,11 @@ public class patter3 {
 
         int n = 4;
         for(int row = 0; row <n; row++ ){
-            System.out.println("");
+          
             for(int col = 0; col <=row ; col++) {
                System.out.print("*");
     }
+      System.out.println(" ");
 }
     }   
 }
