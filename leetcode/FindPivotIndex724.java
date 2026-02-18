3950668f20c0a8aee87cc149f0b287f1ae5917b6
grchetan <chetanprajapat340@gmail.com>
2026-02-18 10:06:59 +0530

    add file

---
+++ leetcode/FindPivotIndex724.java
@@ -0,0 +1,21 @@
+
+
+public class FindPivotIndex724 {
+  public static void main(String[] args) {
+    
+    int[] nums = {3 ,3 ,6, 7, 4};
+    int totalSum = 0;
+     for(int n : nums){
+            totalSum += n;
+        }
+
+
+
+        System.out.println(totalSum);
+  }
+
+  
+
+
+  
+}
