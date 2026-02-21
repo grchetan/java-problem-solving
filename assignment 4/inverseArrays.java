1632884cdba2237b16c016e6b7b27a1562115066
grchetan <chetanprajapat340@gmail.com>
2026-02-21 09:23:53 +0530

    add file

---
+++ assignment 4/inverseArrays.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class inverseArrays {
+
+    public static void inverseArray(int[] arr, int[] inverse, int index) {
+        
+        // Base case
+        if(index == arr.length) {
+            return;
+        }
+
+        // Work
+        inverse[arr[index]] = index;
+
+        // Recursive call
+        inverseArray(arr, inverse, index + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] inverse = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        inverseArray(arr, inverse, 0);
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(inverse[i] + " ");
+        }
+    }
+}
--- assignment 4/inversesNumber.java
+++
@@ -1,6 +0,0 @@
-
-
-public class inversesNumber {
-  
-  
-}
