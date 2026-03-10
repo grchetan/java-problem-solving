dad7e29e359ad2236dcca95c9627d7b10129e6bc
grchetan <chetanprajapat340@gmail.com>
2026-03-10 09:34:06 +0530

    add wave code but not fixed

--- 2d Array/wave.java
+++ 2d Array/wave.java
@@ -1,10 +1,28 @@
 public class wave {
+  public static void wave(int[][] arr, int m,  int n){
+for(int col = 0; col < n; col++){
+  if(col % 2 == 0){
+    for(int row = m - 1; row >=0; row-- ){
+      System.out.print(arr[row][col]+ " ");
+    }
+  }else{
+    for(int row = m -1; row >= 0; row--){
+      System.out.print(arr[row][col]+" ");
+    }
+  }
+
+    }
+  }
   public static void main(String[] args) {
 
     int[][] arr = {{1,2,3,4} , {5,6,7,8}, {9,10,11,12}};
 
-    for(int col = 0; col)
+    int nrow = arr.length;
+    int ncol = arr[0].length;
 
+    wave(arr, nrow, ncol);
+
+    
     
   }
   
