2b5c52359ccc65ff34364e66d7461b664a698c3a
grchetan <chetanprajapat340@gmail.com>
2026-03-23 20:12:46 +0530

    update

--- Basic Array/Selection.java
+++ Basic Array/Selection.java
@@ -2,15 +2,16 @@ public class Selection {
   public static void main(String[] args) {
     
     int[] arr = {2 , 12, 9 , 6 , 7 ,10};
+
     for(int i = 0; i < arr.length -1 ; i++){
       int mini = i;
       
       for(int j = i+1 ; j < arr.length; j++){
-
         if(arr[j] < arr[mini]){
           mini = j;
         }
       }
+      
       // swap array
       int temp = arr[mini];
       arr[mini] = arr[i];
