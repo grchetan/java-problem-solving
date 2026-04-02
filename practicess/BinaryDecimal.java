160a494373de5356d44de212cced4523624a8c72
grchetan <chetanprajapat340@gmail.com>
2026-04-02 09:44:06 +0530

    add binarDecimalcdoe

--- practicess/BinaryDecimal.java
+++ practicess/BinaryDecimal.java
@@ -4,10 +4,37 @@ public class BinaryDecimal {
     Scanner sc = new Scanner(System.in);
 
     int n = sc.nextInt();
-    int dec = 0;
+  
+
+    String bin = "";
+   
+   
 
 
-    for(in)
+    while(n > 0) {
+      int r = n % 2;
+      n = n / 2 ;
+      bin = r + bin;
+    }
+
+    System.out.println("the binary number is :" + bin);
+
+    
+    int binary = Integer.parseInt(bin);
+
+
+    int power = 0 , d = 0;
+    
+    while(binary  > 0) {
+      int r = binary % 10;
+      d = d + r * (int) Math.pow(2, power);
+      binary = binary / 10 ;
+      power++;
+    }
+
+    System.out.println(d);
+
+
     
   }
   
