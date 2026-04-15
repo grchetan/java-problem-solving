5c9d11eec20da27dbb42304e4ad3b268159ad2c7
grchetan <chetanprajapat340@gmail.com>
2026-04-15 22:51:29 +0530

    add hashmap file

--- src/hashmap/basic.java
+++ src/hashmap/basic.java
@@ -4,13 +4,28 @@ import java.util.*;
 public class basic {
     public static void main(String[] args) {
 
-        int[] nums = {1, 2, 2, 3, 1};
+        int[] nums = {1, 2, 2, 3, 5};
+        int k = 2;
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        // for(int num : nums){
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
+
+
+        for(int i = 0; i < nums.length; i++){
+            int num = nums[i];
+
+            if(map.containsKey(num)){
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
         }
+
+
+
 
         System.out.println(map);
     }
